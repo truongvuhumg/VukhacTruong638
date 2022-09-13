df052139c2a12d0a9d478bc190cd47afd5e4e572
truongvuhumg <truongkhac0607@gmail.com>
2022-09-13 23:24:55 +0700

    VKT_638

---
+++ Models/employee.cs
@@ -0,0 +1,9 @@
+namespace VuKhacTruongBTH.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string HoTen { get; set; }
+        public int EmployeeAge { get; set; }
+    }
+}
---
+++ Models/student.cs
@@ -0,0 +1,9 @@
+namespace DemoMVC.Models
+{
+    public class Student 
+    {
+        public int StudentID {get;set;}
+        }
+
+}
+
